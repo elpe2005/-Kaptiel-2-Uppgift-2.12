3b8ae6add226e55636ff3303c4fe65fe4ad8d0cf
elpe2005 <epedersen2005@gmail.com>
2022-09-14 11:55:43 +0200

    klar med uppgift 1.12

--- Uppgift 2.12/Program.cs
+++ Uppgift 2.12/Program.cs
@@ -5,7 +5,18 @@ namespace Uppgift_2._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Startavgift 300 kr");
+            Console.WriteLine("Välkommen till bil hyrningsfirman, här kan du räkna hur mycket det kommer kosta. ");
+            Console.WriteLine(" Hur många dagar ska du hyra? ");
+            string dagar = Console.ReadLine();
+            Console.WriteLine(" Okej hur många kilometer ska du åka?");
+            string km = Console.ReadLine();
+            int dagarna = int.Parse(dagar);
+            int kmarna = int.Parse(km);
+            int summan = 300 + kmarna + 500 * dagarna;
+            Console.WriteLine("Summan är " + summan + "kr");
+
+            
+            
         }
     }
 }
